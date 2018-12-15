b4a6fa42a15c1cab166294096e8a2635b49c15e5
Rishi <avvarurishi123@gmail.com>
2018-12-14 19:30:28 -0800

    added xcode

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Rishi bharadwaj Avvaru on 12/11/18.
+//  Copyright © 2018 Rishias12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var numberOnScreen : Double = 0;
+    var operation = 0
+    var previousNumber : Double = 0;
+    var performingMath = false
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numberButton(_ sender: UIButton) {
+        
+        if performingMath == true{
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+            
+        }
+        
+        else{
+            label.text = label.text! + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+        }
+    }
+    
+    @IBAction func buttons(_ sender: UIButton)
+    {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16{
+           
+            previousNumber = Double(label.text!)!
+            if sender.tag == 12  // divide
+            {
+                label.text = "/"
+            }
+            else if sender.tag == 13 //multiple
+            {
+                label.text = "*"
+            }
+            else if sender.tag == 14 // minus
+            {
+                label.text = "-"
+            }
+            else if sender.tag == 15 // plus
+            {
+                label.text = "+"
+            }
+            operation = sender.tag
+            performingMath = true;
+        }
+        
+        else if sender.tag == 16
+        {
+            if operation == 12
+            {
+                label.text = String(previousNumber / numberOnScreen)
+            }
+            else if operation == 13
+            {
+                label.text = String(previousNumber * numberOnScreen)
+            }
+            else if operation == 14
+            {
+                label.text = String(previousNumber - numberOnScreen)
+            }
+           else if operation == 15
+            {
+                label.text = String(previousNumber + numberOnScreen)
+            }
+        }
+        else if sender.tag == 11
+        {
+            label.text = ""
+            previousNumber = 0;
+            numberOnScreen = 0;
+            operation = 0;
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+    
+    
+}
+
